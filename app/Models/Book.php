f258a43acb4c7e67c833033e2782e4d4949c28b4
Shahbaj <shahbaj.husen@neosoftmail.com>
2024-10-03 11:46:24 +0530

    Creating controllers and models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,7 +13,39 @@ class Book extends Model
     #book is parent of review table, one book can have multiple reviews
     protected $fillable = ['title', 'author'];
 
-    public function review(){
-       return $this->hasMany(Review::class);
+    public function review()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('review')
+            ->orderBy('review_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        // return $query->withRecentReviews(fn($date) => $date->subWeek());
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
